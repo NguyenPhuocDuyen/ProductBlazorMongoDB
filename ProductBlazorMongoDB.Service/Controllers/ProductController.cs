60ee7610d4a853606ab33bd775a0a8e46868f28a
DuyenNP <DuyenNP7901@gmail.com>
2023-12-19 14:38:07 +0700

    add RESTfull api category and product

---
+++ ProductBlazorMongoDB.Service/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductBlazorMongoDB.Service.Models;
+using ProductBlazorMongoDB.Service.Services;
+
+namespace ProductBlazorMongoDB.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // GET: api/Product
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var products = await _productService.GetAllAsync();
+            return Ok(products);
+        }
+
+        // GET api/Product/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var product = await _productService.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        // POST api/Product
+        [HttpPost]
+        public async Task<IActionResult> Post(Product product)
+        {
+            product.CategoryName = null;
+            await _productService.CreateAsync(product);
+            return Ok("created successfully");
+        }
+
+        // PUT api/Product/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(string id, [FromBody] Product newProduct)
+        {
+            newProduct.CategoryName = null;
+            var product = await _productService.GetById(id);
+            if (product == null)
+                return NotFound();
+            await _productService.UpdateAsync(id, newProduct);
+            return Ok("updated successfully");
+        }
+
+        // DELETE api/Product/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var product = await _productService.GetById(id);
+            if (product == null)
+                return NotFound();
+            await _productService.DeleteAsync(id);
+            return Ok("deleted successfully");
+        }
+    }
+}
